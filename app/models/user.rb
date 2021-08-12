da8d26f2fa68f1957c3d31f1170db9a880741ff0
harsh291199 <harshsuthar2911@gmail.com>
2021-08-12 16:30:17 +0530

    Add Message model and Add Association between User and Message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 # User model
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { in: 3..15 }
+  validates :username, presence: true, length: { in: 3..15 }, uniqueness: { case_sensitive: false }
+  has_many :messages
   has_secure_password
 end
---
+++ db/migrate/20210812104737_create_messages.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Create Messages
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
