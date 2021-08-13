ef463af107c09aeee30fb76fe2429cbd8423aef4
harsh291199 <harshsuthar2911@gmail.com>
2021-08-13 16:50:00 +0530

    Display messages realtime

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,14 +6,19 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
-    if message.save
-      ActionCable.server.broadcast 'chatroom_channel', foo: message.body
-    end
+    return unless message.save
+
+    ActionCable.server.broadcast 'chatroom_channel',
+                                 message: message_render(message)
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,6 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log(data.foo);
+    $('#message-container').append(data.message)
   }
 });
