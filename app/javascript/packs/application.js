ffc388b773c32cef470c9fd406dbc106ddf47c2a
harsh291199 <harshsuthar2911@gmail.com>
2021-08-13 17:51:21 +0530

    Update message scope and submission

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,5 +20,11 @@ $(document).on('turbolinks:load', function() {
  if ($('#messages').length > 0) {
   $('#messages').scrollTop($('#messages')[0].scrollHeight);
  }
+ $('#message_body').on('keydown', function(e){
+  if (e.keyCode == 13){
+    $('button').click();
+    e.target.value = "";
+  };
+ });
 })
 
